8642d4277660dde9bdd5c04b350e34767fcd5108
Sophia Di <backwardsxmarathon@gmail.com>
2017-06-05 15:45:50 -0400

    Add Blogs component which renders blogs returned from axios request to blog-backend rails API.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './components/App';
+import Blogs from './components/Blogs';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Blogs />, document.getElementById('root'));
 registerServiceWorker();
