b4f3a242eae26452aac0bc38ea978469cbd7c1a7
Sophia Di <backwardsxmarathon@gmail.com>
2017-06-05 15:06:55 -0400

    Initial commit. Sets up proxy server in order to communicate with blog back-end.

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  render() {
+
+    //   package.json has proxy: "http://localhost:3000/"
+      axios.get("/blogs").then(function(response) {
+          console.log(response);
+      });
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
